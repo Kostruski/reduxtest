40b23d470088f4cdaec21b2853fe1876840f0f6a
Marek <mkkos123@gmail.com>
2019-10-29 16:44:35 +0100

    add to app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
 
 import Counter from './components/Counter';
-<<<<<<< Updated upstream
-=======
 import './App.scss';
->>>>>>> Stashed changes
+
 
 const App = props => {
   return (
-<<<<<<< Updated upstream
-    <div class Name='App'>
-=======
     <div className='App'>
->>>>>>> Stashed changes
       <Counter />
     </div>
   );
