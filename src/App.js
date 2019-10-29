3af6b477d1dd65968637902ec8a9ddc57b93f016
Marek <mkkos123@gmail.com>
2019-10-29 16:43:32 +0100

    add dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import './App.scss';
 
 import Counter from './components/Counter';
+<<<<<<< Updated upstream
+=======
+import './App.scss';
+>>>>>>> Stashed changes
 
-const App = () => {
+const App = props => {
   return (
+<<<<<<< Updated upstream
     <div class Name='App'>
+=======
+    <div className='App'>
+>>>>>>> Stashed changes
       <Counter />
     </div>
   );
